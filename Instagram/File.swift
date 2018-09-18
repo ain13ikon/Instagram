8d000713d632ba7f85241f379b5fc7af8ac55d1c
ain <ain.git13@gmail.com>
2018-09-18 13:56:15 +0900

    完成：基本動作

---
+++ Instagram/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Instagram
+//
+//  Created by きたむら on 2018/09/18.
+//  Copyright © 2018年 ain13ikon. All rights reserved.
+//
+
+import Foundation
---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,86 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by きたむら on 2018/09/18.
+//  Copyright © 2018年 ain13ikon. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    //投稿データをセットする
+    func setPostData(_ postData: PostData){
+        //画像
+        self.postImageView.image = postData.image
+        
+        //キャプション
+        self.captionLabel.text = "\(postData.name!) : \(postData.caption!)"
+        
+        //いいね
+        let likeNumber = postData.likes.count
+        likeLabel.text = "\(likeNumber)"
+        
+        //日付
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        let dateString = formatter.string(from: postData.date!)
+        self.dateLabel.text = dateString
+        
+        if postData.isLiked {
+            //いいねされている場合
+            let buttonImage = UIImage(named: "like_exist")
+            self.likeButton.setImage(buttonImage, for: .normal)
+        }else{
+            //いいねされていない場合
+            let buttonImage = UIImage(named: "like_none")
+            self.likeButton.setImage(buttonImage, for: .normal)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
